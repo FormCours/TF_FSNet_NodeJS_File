a644e92cee1db714cb6ad22dfcf83079fae9eed1
Aurélien <formation.net@outlook.be>
2020-10-02 12:40:41 +0200

    Methode de lecture du JSON

---
+++ modules/people-helper.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+
+const getPeople = (jsonFile) => {
+
+    return new Promise((resolve, reject) => {
+
+        fs.readFile(jsonFile, (error, data) => {
+            if(error) {
+                reject('Erreur avec le fichier')
+            }
+            else {
+                try {
+                    const jsonData = JSON.parse(data);
+        
+                    resolve(jsonData.people); 
+                } 
+                catch (errorJson) {
+                    reject('Erreur de syntaxe JSON');
+                }
+            }
+        });
+    });
+}
